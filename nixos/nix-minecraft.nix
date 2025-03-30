424a0b17342e68009fa53dda285d263ac290841c
TristenYim <unfathomy@proton.me>
2025-03-30 01:40:57 -0700

    Added modpack to server
    This is based on the packwiz modpack in this very repo. It needs to be
    updated (and configured to not crash), but it "technically works" right
    now.

--- nixos/nix-minecraft.nix
+++ nixos/nix-minecraft.nix
@@ -9,11 +9,21 @@
         eula = true; # Accepting the EULA is required
 
         servers = {
-            Astrals-SMP = {
+            Astrals-SMP = let
+                modpack = pkgs.fetchPackwizModpack {
+                    url = "https://raw.githubusercontent.com/TristenYim/Astrals-SMP/refs/heads/master/Astrals-SMP-Full/pack.toml";
+                    packHash = "sha256-xqMAzEU0Y3Mr/Cc5jo2Tr3HLlA4ZHtsNrtJjjqNWMDo=";
+                    side = "both";
+                };
+            in {
                 enable = true;
 
                 # Define the server jar version.
-                package = pkgs.fabricServers.fabric-1_20_1;
+                package = pkgs.fabricServers.fabric-1_20_1.override { loaderVersion = "0.16.0"; };
+
+                symlinks = {
+                    "mods" = "${modpack}/mods";
+                };
 
                 # This option opens up the firewall, allowing users from the local network (or global network if port forwarding is configured) to connect. Be careful!
                 # openFirewall = true;
